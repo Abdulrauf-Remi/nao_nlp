7d9801716d010d83961427b19d028c225bcddb84
Abdulrauf_Remi <anadeenara@gmail.com>
2022-09-15 21:40:47 +0000

    new

--- chat.py
+++ chat.py
@@ -32,15 +32,21 @@ while True:
     if sentence == "quit":
         break
 
-sentence = tokenize(sentence)
-X = bag_of_words(sentence, all_worlds)
-X = X.reshape(1, X.shape[0])
-X = torch.from_numpy()
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_worlds)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X)
 
-output = model(X)
-_, predicted = torch.max(output, dim=1)
-tag = tags[predicted.item()]
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
 
-for intent in intents["intents"]:
-    if tag == intent["tag"]:
-        print(f"{bot_name}: {random.choice(intent["responses"])}")
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents["intents"]:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: I do not understand...")
